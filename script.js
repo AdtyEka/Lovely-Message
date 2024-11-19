832f20c57bda2f1560f97bb8578d4d071db37f34
AdtyaEkaa <eka255765@gmail.com>
2024-11-19 21:19:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('envelope').addEventListener('click', function() {
+    this.style.display = 'none';
+    document.getElementById('message-container').style.display = 'block';
+});
